a3d24b4409d2e0252bb723b7560f5d11728ab457
YoussefPasha <jojoyossefbasha010@gmail.com>
2021-01-07 18:30:33 +0200

    add text to slider

--- src/Authentication/Onboarding/Onboarding.tsx
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { View, ScrollView, StyleSheet, Dimensions } from "react-native";
 
-import Slide from "./Slide";
+import Slide, { SLIDE_HEIGHT } from "./Slide";
 
-const { width, height } = Dimensions.get("window");
+const { width } = Dimensions.get("window");
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "white",
   },
   slider: {
-    flex: 0.61,
+    height: SLIDE_HEIGHT,
+    backgroundColor: "cyan",
+    borderBottomRightRadius: 75,
   },
   footer: {
     flex: 1,
@@ -20,14 +23,27 @@ const Onboarding = () => {
   return (
     <View style={styles.container}>
       <View style={styles.slider}>
-        <ScrollView horizontal snapToInterval={width} decelerationRate="fast">
+        <ScrollView
+          horizontal
+          snapToInterval={width}
+          showsHorizontalScrollIndicator={false}
+          bounces={false}
+          decelerationRate="fast"
+        >
           <Slide label="Relaxed" />
           <Slide label="Playful" right />
-          <Slide label="Exocentric" />
+          <Slide label="Excentric" />
           <Slide label="Funky" right />
         </ScrollView>
       </View>
-      <View style={styles.footer} />
+      <View style={styles.footer}>
+        <View
+          style={{ ...StyleSheet.absoluteFillObject, backgroundColor: "cyan" }}
+        />
+        <View
+          style={{ flex: 1, backgroundColor: "white", borderTopLeftRadius: 75 }}
+        />
+      </View>
     </View>
   );
 };
